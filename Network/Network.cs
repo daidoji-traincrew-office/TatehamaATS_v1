5a79afbf2fe82a9556e83db06b27e00176ed55b2
TakumiteTudo <takumit2886@gmail.com>
2025-04-17 19:40:25 +0900

    上下線判定部の調整

--- Network/Network.cs
+++ Network/Network.cs
@@ -338,7 +338,14 @@ namespace TatehamaATS_v1.Network
         {
             try
             {
-                bool currentStatus = IsOddTrainNumber(OverrideDiaName) != IsOddTrainNumber(TcData.myTrainData.diaName);
+                bool currentStatus = true;
+                try
+                {
+                    currentStatus = IsOddTrainNumber(OverrideDiaName) != IsOddTrainNumber(TcData.myTrainData.diaName);
+                }
+                catch
+                {
+                }
 
                 if (currentStatus)
                 {
