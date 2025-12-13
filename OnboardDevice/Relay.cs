35f462af5c126adc7d9e77de55648c2bce9fc520
TakumiteTudo <takumit2886@gmail.com>
2025-12-13 17:29:13 +0900

    GameScreen関係の判定を修正

--- OnboardDevice/Relay.cs
+++ OnboardDevice/Relay.cs
@@ -426,7 +426,7 @@ namespace TatehamaATS_v1.OnboardDevice
 
         internal void SignalSet(List<SignalData> signalDatas)
         {
-            if (!(TcData.gameScreen.HasFlag(TrainCrewAPI.GameScreen.MainGame) || TcData.gameScreen.HasFlag(TrainCrewAPI.GameScreen.MainGame_Pause)))
+            if (!(TcData.gameScreen == TrainCrewAPI.GameScreen.MainGame || TcData.gameScreen == TrainCrewAPI.GameScreen.MainGame_Pause))
             {
                 return;
             }
@@ -505,7 +505,7 @@ namespace TatehamaATS_v1.OnboardDevice
 
         internal void UpdateRoute(List<Route> routes)
         {
-            if (!(TcData.gameScreen.HasFlag(TrainCrewAPI.GameScreen.MainGame) || TcData.gameScreen.HasFlag(TrainCrewAPI.GameScreen.MainGame_Pause)))
+            if (!(TcData.gameScreen == TrainCrewAPI.GameScreen.MainGame || TcData.gameScreen == TrainCrewAPI.GameScreen.MainGame_Pause))
             {
                 return;
             }
