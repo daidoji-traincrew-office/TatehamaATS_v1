6a1af7352311713aac8ae102be09b2828fa216b7
gimyonabetechi <gimyo.nabetateyamatonnel+github@gmail.com>
2026-03-22 02:07:03 +0900

    ATS表示器をリサイズ可能に

--- ATSDisplay/LEDWindow.cs
+++ ATSDisplay/LEDWindow.cs
@@ -1,21 +1,38 @@
 ﻿using TatehamaATS_v1.Exceptions;
+using TatehamaATS_v1.KokuchiWindow;
 
 namespace TatehamaATS_v1.ATSDisplay
 {
-    public partial class LEDWindow : Form
-    {
+    public partial class LEDWindow : Form {
         private Bitmap sourceImage;
         internal event Action LEDTestModePush;
 
-        public LEDWindow()
-        {
+        private Size originSize = new Size(277, 360);
+        private Size displaySize = new Size(277, 360);
+        private Point panelLocation = new Point(40, 35);
+        private Size panelSize = new Size(289, 97);
+        private Point l1Location = new Point(40, 35);
+        private Point l2Location = new Point(40, 35);
+        private Point l3Location = new Point(40, 35);
+        private Size ledSize = new Size(289, 97);
+        private Point buttonLocation = new Point(219, 331);
+
+        public LEDWindow() {
             InitializeComponent();
             sourceImage = LEDResource.ATS_LED;
+            originSize = ClientSize;
+            displaySize = originSize;
+            panelLocation = panel1.Location;
+            panelSize = panel1.Size;
+            l1Location = L1.Location;
+            l2Location = L2.Location;
+            l3Location = L3.Location;
+            ledSize = L1.Size;
+            buttonLocation = LEDTest.Location;
             Shown += TopMost_Shown;
         }
 
-        private void TopMost_Shown(Object? sender, EventArgs e)
-        {
+        private void TopMost_Shown(Object? sender, EventArgs e) {
             TopLevel = false;
             TopLevel = true;
             TopMost = false;
@@ -26,13 +43,10 @@ namespace TatehamaATS_v1.ATSDisplay
         /// </summary>
         /// <param name="pictureBoxIndex">表示するPictureBoxの番号（1～3）</param>
         /// <param name="imageNumber">表示する画像の番号</param>
-        internal void DisplayImage(int pictureBoxIndex, int imageNumber)
-        {
-            try
-            {
+        internal void DisplayImage(int pictureBoxIndex, int imageNumber) {
+            try {
                 Bitmap croppedImage;
-                if (0x180 <= imageNumber && imageNumber <= 0x1FF || 0x280 <= imageNumber && imageNumber <= 0x2FF || 0x380 <= imageNumber && imageNumber <= 0x3FF || 0x580 <= imageNumber && imageNumber <= 0x5FF || 0x680 <= imageNumber && imageNumber <= 0x6FF || 0x780 <= imageNumber && imageNumber <= 0x7FF || 0x880 <= imageNumber && imageNumber <= 0x8FF)
-                {
+                if (0x180 <= imageNumber && imageNumber <= 0x1FF || 0x280 <= imageNumber && imageNumber <= 0x2FF || 0x380 <= imageNumber && imageNumber <= 0x3FF || 0x580 <= imageNumber && imageNumber <= 0x5FF || 0x680 <= imageNumber && imageNumber <= 0x6FF || 0x780 <= imageNumber && imageNumber <= 0x7FF || 0x880 <= imageNumber && imageNumber <= 0x8FF) {
                     croppedImage = GetImageByNumber(351);
                     //コード表示無視
                     int codeC = (imageNumber >> 8) & 0xF;
@@ -42,15 +56,13 @@ namespace TatehamaATS_v1.ATSDisplay
                     int codeA = imageNumber & 0xF;
                     Bitmap codeAImage = GetImageByCodeNumber(codeA);
 
-                    using (Graphics g = Graphics.FromImage(croppedImage))
-                    {
+                    using (Graphics g = Graphics.FromImage(croppedImage)) {
                         g.DrawImage(codeAImage, 26, 0, codeAImage.Width, codeAImage.Height);
                         g.DrawImage(codeBImage, 19, 0, codeBImage.Width, codeBImage.Height);
                         g.DrawImage(codeCImage, 12, 0, codeCImage.Width, codeCImage.Height);
                     }
                 }
-                else
-                {
+                else {
                     croppedImage = GetImageByNumber(imageNumber);
                 }
                 PictureBox pictureBox = GetPictureBoxByIndex(pictureBoxIndex);
@@ -59,8 +71,7 @@ namespace TatehamaATS_v1.ATSDisplay
 
                 pictureBox.BackgroundImage = enlargedImage;
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 throw new LEDControlException(3, $"エラーが発生しました: {ex.Message} @DisplayImage", ex);
             }
         }
@@ -70,24 +81,18 @@ namespace TatehamaATS_v1.ATSDisplay
         /// </summary>
         /// <param name="original">元の画像</param>
         /// <returns>6倍に拡大された画像</returns>
-        private Bitmap EnlargePixelArt(Bitmap original)
-        {
+        private Bitmap EnlargePixelArt(Bitmap original) {
             int newWidth = original.Width * 6;
             int newHeight = original.Height * 6;
 
             Bitmap enlargedImage = new Bitmap(newWidth + 1, newHeight + 1);
-            using (Graphics g = Graphics.FromImage(enlargedImage))
-            {
+            using (Graphics g = Graphics.FromImage(enlargedImage)) {
                 g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
-                for (int y = 0; y < original.Height; y++)
-                {
-                    for (int x = 0; x < original.Width; x++)
-                    {
+                for (int y = 0; y < original.Height; y++) {
+                    for (int x = 0; x < original.Width; x++) {
                         Color pixelColor = original.GetPixel(x, y);
-                        for (int dy = 0; dy < 6; dy++)
-                        {
-                            for (int dx = 0; dx < 6; dx++)
-                            {
+                        for (int dy = 0; dy < 6; dy++) {
+                            for (int dx = 0; dx < 6; dx++) {
                                 enlargedImage.SetPixel(x * 6 + dx, y * 6 + dy, pixelColor);
                             }
                         }
@@ -104,8 +109,7 @@ namespace TatehamaATS_v1.ATSDisplay
         /// </summary>
         /// <param name="number">切り出す画像の番号</param>
         /// <returns>切り出された画像</returns>
-        private Bitmap GetImageByNumber(int number)
-        {
+        private Bitmap GetImageByNumber(int number) {
             int columns = 8;
             int rows = 32;
             int width = 32;
@@ -115,8 +119,7 @@ namespace TatehamaATS_v1.ATSDisplay
             int colIndex = number / 50;
             int rowIndex = number % 50;
 
-            if (colIndex >= columns || rowIndex >= rows)
-            {
+            if (colIndex >= columns || rowIndex >= rows) {
                 throw new LEDDisplayNumberAbnormal(3, $"指定された番号{number}が無効です", new ArgumentOutOfRangeException());
             }
 
@@ -124,8 +127,7 @@ namespace TatehamaATS_v1.ATSDisplay
             int y = margin + rowIndex * (height + margin);
 
             Bitmap croppedImage = new Bitmap(width, height);
-            using (Graphics g = Graphics.FromImage(croppedImage))
-            {
+            using (Graphics g = Graphics.FromImage(croppedImage)) {
                 g.DrawImage(sourceImage, new Rectangle(0, 0, width, height), new Rectangle(x, y, width, height), GraphicsUnit.Pixel);
             }
 
@@ -137,8 +139,7 @@ namespace TatehamaATS_v1.ATSDisplay
         /// </summary>
         /// <param name="number">切り出す画像の番号</param>
         /// <returns>切り出された画像</returns>
-        private Bitmap GetImageByCodeNumber(int number)
-        {
+        private Bitmap GetImageByCodeNumber(int number) {
             int columns = 4;
             int rows = 4;
             int width = 6;
@@ -151,8 +152,7 @@ namespace TatehamaATS_v1.ATSDisplay
             int colIndex = number / 4;
             int rowIndex = number % 4;
 
-            if (colIndex >= columns || rowIndex >= rows)
-            {
+            if (colIndex >= columns || rowIndex >= rows) {
                 throw new LEDDisplayNumberAbnormal(3, $"指定された番号{number}が無効です", new ArgumentOutOfRangeException());
             }
 
@@ -160,8 +160,7 @@ namespace TatehamaATS_v1.ATSDisplay
             int y = dy + margin + rowIndex * (height + margin);
 
             Bitmap croppedImage = new Bitmap(width, height);
-            using (Graphics g = Graphics.FromImage(croppedImage))
-            {
+            using (Graphics g = Graphics.FromImage(croppedImage)) {
                 g.DrawImage(sourceImage, new Rectangle(0, 0, width, height), new Rectangle(x, y, width, height), GraphicsUnit.Pixel);
             }
 
@@ -173,10 +172,8 @@ namespace TatehamaATS_v1.ATSDisplay
         /// </summary>
         /// <param name="index">PictureBoxの番号（1～3）</param>
         /// <returns>対応するPictureBox</returns>
-        private PictureBox GetPictureBoxByIndex(int index)
-        {
-            switch (index)
-            {
+        private PictureBox GetPictureBoxByIndex(int index) {
+            switch (index) {
                 case 1:
                     return L1;
                 case 2:
@@ -197,10 +194,8 @@ namespace TatehamaATS_v1.ATSDisplay
         /// <summary>
         /// マウスダウンイベントの処理
         /// </summary>
-        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
                 dragStartPoint = e.Location;
             }
         }
@@ -208,17 +203,57 @@ namespace TatehamaATS_v1.ATSDisplay
         /// <summary>
         /// マウスムーブイベントの処理
         /// </summary>
-        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
                 this.Location = new Point(this.Left + e.X - dragStartPoint.X, this.Top + e.Y - dragStartPoint.Y);
             }
         }
 
-        private void LEDTest_Click(object sender, EventArgs e)
-        {
+        private void LEDTest_Click(object sender, EventArgs e) {
             LEDTestModePush?.Invoke();
+        }
+
+        private void LEDWindow_Activated(object sender, EventArgs e) {
+            var loc = Location;
+            FormBorderStyle = FormBorderStyle.Sizable;
+            var borderWidth = Size.Width - ClientSize.Width;
+            var borderHeight = Size.Height - ClientSize.Height;
+            Location = new Point(loc.X - borderWidth / 2, loc.Y - borderHeight + borderWidth / 2);
+        }
+
+        private void LEDWindow_Deactivate(object sender, EventArgs e) {
+            var borderWidth = Size.Width - ClientSize.Width;
+            var borderHeight = Size.Height - ClientSize.Height;
+            FormBorderStyle = FormBorderStyle.None;
+            Location = new Point(Location.X + borderWidth / 2, Location.Y + borderHeight - borderWidth / 2);
+        }
+
+        private void LEDWindow_ResizeEnd(object sender, EventArgs e) {
+            var newWidth = ClientSize.Width;
+            var newHeight = ClientSize.Height;
+            if ((float)originSize.Width / originSize.Height < (float)newWidth / newHeight) {
+                newWidth = originSize.Width * newHeight / originSize.Height;
+            }
+            else if ((float)originSize.Width / originSize.Height > (float)newWidth / newHeight) {
+                newHeight = originSize.Height * newWidth / originSize.Width;
+            }
+            displaySize = new Size(newWidth, newHeight);
+            Size = new Size(newWidth + Size.Width - ClientSize.Width, newHeight + Size.Height - ClientSize.Height);
+
+
+            panel1.Location = new Point(panelLocation.X * newHeight / originSize.Height, panelLocation.Y * newHeight / originSize.Height);
+            panel1.Size = new Size(panelSize.Width * newHeight / originSize.Height, panelSize.Height * newHeight / originSize.Height);
+
+            L1.Location = new Point(l1Location.X * newHeight / originSize.Height, l1Location.Y * newHeight / originSize.Height);
+            L1.Size = new Size(ledSize.Width * newHeight / originSize.Height, ledSize.Height * newHeight / originSize.Height);
+
+            L2.Location = new Point(l2Location.X * newHeight / originSize.Height, l2Location.Y * newHeight / originSize.Height);
+            L2.Size = new Size(ledSize.Width * newHeight / originSize.Height, ledSize.Height * newHeight / originSize.Height);
+
+            L3.Location = new Point(l3Location.X * newHeight / originSize.Height, l3Location.Y * newHeight / originSize.Height);
+            L3.Size = new Size(ledSize.Width * newHeight / originSize.Height, ledSize.Height * newHeight / originSize.Height);
+
+            LEDTest.Location = new Point(buttonLocation.X * newHeight / originSize.Height, buttonLocation.Y * newHeight / originSize.Height);
         }
     }
 }
